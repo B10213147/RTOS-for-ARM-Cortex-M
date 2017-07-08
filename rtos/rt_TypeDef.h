e726f6bc393dc41f12880437aac55844d5ddbd60
B10213147 <B10213147@yuntech.org.tw>
2017-07-08 18:01:15 +0800

    Added rt_TypeDef.h
    v0.2.1

---
+++ rtos/rt_TypeDef.h
@@ -0,0 +1,26 @@
+/*
+ * rt_TypeDef.h
+ *
+ *  Created on: July 8, 2017
+ *      Author: Harvard Tseng
+ */
+ 
+#ifndef RT_TYPEDEF_H_
+#define RT_TYPEDEF_H_
+
+typedef void (*voidfuncptr)(void);
+
+struct OS_TCB{
+    
+};
+
+struct OS_TSK{
+    
+};
+
+struct OS_MCB{
+    
+};
+
+
+#endif /* RT_TYPEDEF_H_ */
