383a3fb070c6be4b22e44e8d1df51ebd89ffed01
B10213147 <B10213147@yuntech.org.tw>
2017-07-09 23:38:24 +0800

    Added memory control block struct
    v1.2.1

--- rtos/main.c
+++ rtos/main.c
@@ -1,12 +1,12 @@
 #include "rtos.h"
 
-void test1(void){
-    OS_Task_Create(__empty);
-}
-void test2(void){
-    OS_Task_Delete(__empty);
-}
+void test1(void);
+void test2(void);
 
+char data_char[10];
+OS_MCB test1_fifo = {0, 0, 10, data_char};
+int data_int[5];
+OS_MCB test2_fifo = {0, 0, 5, data_int};
 int main(void){
     OS_Task_Create(test1);    
     OS_Task_Create(__empty);
@@ -15,4 +15,11 @@ int main(void){
     OS_Init(1000, CM_SysTick);  // Time slice = 1ms
     while(1);
     return 0;
+}
+
+void test1(void){
+    
+}
+void test2(void){
+    
 }
---
+++ rtos/rt_mailbox.h
@@ -0,0 +1,14 @@
+/*
+ * rt_mailbox.h
+ *
+ *  Created on: July 10, 2017
+ *      Author: Harvard Tseng
+ */
+ 
+#ifndef RT_MAILBOX_H_
+#define RT_MAILBOX_H_
+
+#include "rt_TypeDef.h"
+
+
+#endif /* RT_MAILBOX_H_ */
