9d91256726fce6515a485eff8d25ef5d05b47e9e
B10213147 <B10213147@yuntech.org.tw>
2017-07-13 10:43:43 +0800

    Added rt_memory files
    v1.6.1

---
+++ rtos/rt_memory.h
@@ -0,0 +1,14 @@
+/*
+ * rt_memory.h
+ *
+ *  Created on: July 13, 2017
+ *      Author: Harvard Tseng
+ */
+ 
+#ifndef RT_MEMORY_H_
+#define RT_MEMORY_H_
+
+#include "rt_TypeDef.h"
+
+
+#endif /* RT_MEMORY_H_ */
