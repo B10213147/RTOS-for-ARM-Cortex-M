6a93db6167b65bb63aec36c8799eac1daa60351d
B10213147 <B10213147@yuntech.org.tw>
2017-07-13 11:03:44 +0800

    Added new types
    v1.6.2

--- rtos/rt_memory.c
+++ rtos/rt_memory.c
@@ -9,6 +9,16 @@
 #include "rt_memory.h"
 
 /* Private typedef -----------------------------------------------------------*/
+typedef struct mem_block{
+    struct mem_block *next;
+    unsigned int size;
+}*P_MEMB;
+
+typedef struct mem{
+    P_MEMB free;
+    P_MEMB used;
+}*P_MEM;
+
 /* Private define ------------------------------------------------------------*/ 
 /* Private macro -------------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
