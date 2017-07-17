8bf6012a5a7aaae976ee9e0d5e8264227f0afa9e
B10213147 <B10213147@yuntech.org.tw>
2017-07-17 22:58:50 +0800

    Added rt_pool_alloc()
    v2.1.4

--- rtos/rt_mempool.c
+++ rtos/rt_mempool.c
@@ -47,3 +47,16 @@ void rt_pool_distroy(P_POOL pool){
     rt_mem_free(&system_memory, pool);
     OSEnable();
 }
+
+void *rt_pool_alloc(P_POOL pool){
+    int free_id;
+    char *mem = NULL;
+    for(free_id = 0; free_id < pool->blocks; free_id++){
+        if(pool->active_id[free_id]){ break; }
+    }
+    if(free_id < pool->blocks){
+        pool->active_id[free_id] = 1;
+        mem = pool->pool + free_id * pool->size;
+    }
+    return mem;
+}
