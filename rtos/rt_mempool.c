fe482f66cc62b42804bbe6bf7d4aa12010643c3f
B10213147 <B10213147@yuntech.org.tw>
2017-07-17 22:48:38 +0800

    Added rt_pool_distroy()
    v2.1.3

--- rtos/rt_mempool.c
+++ rtos/rt_mempool.c
@@ -19,11 +19,16 @@
 
 P_POOL rt_pool_create(uint32_t size, uint32_t blocks){
     P_POOL pool;
+    OSDisable();
+    
     // 4-byte alignment
     size = (size + 3U) & ~3U;
     pool = (P_POOL)rt_mem_alloc(&system_memory, 
         sizeof(struct mempool) + blocks * (size + 1));
-    if(!pool){ return NULL; }
+    if(!pool){ 
+        OSEnable();
+        return NULL; 
+    }
     pool->size = size;
     pool->blocks = blocks;
     pool->active_id = (char *)((uint32_t)pool + sizeof(struct mempool));
@@ -32,5 +37,13 @@ P_POOL rt_pool_create(uint32_t size, uint32_t blocks){
     for(int i = 0; i < blocks; i++){
         pool->active_id[i] = 0;
     }
+    
+    OSEnable();
     return pool;
+}
+
+void rt_pool_distroy(P_POOL pool){
+    OSDisable();
+    rt_mem_free(&system_memory, pool);
+    OSEnable();
 }
